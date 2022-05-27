8d4941a127dcbfb4c00d90f18a784ff889dbe004
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-05-27 02:14:24 +0200

    Added play page

--- src/Pages/Play.tsx
+++ src/Pages/Play.tsx
@@ -2,6 +2,9 @@ import type { FC } from "react";
 import { useSpring, animated } from "react-spring";
 import Page from "../Components/Page/Page";
 import Header from "../Components/Header/Header";
+import Button from "../Components/Button/Button";
+import Text from "../Components/Text/Text";
+import Icon from "../Components/Icon/Icon";
 
 const Play: FC = (): JSX.Element => {
   const header = useSpring({
@@ -13,8 +16,20 @@ const Play: FC = (): JSX.Element => {
 
   return (
     <Page>
-      <animated.div style={header}>
+      <animated.div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+          ...header,
+        }}
+      >
         <Header fontSize={4}>Play</Header>
+        <Button to="/">
+          <Icon>&#xE00E;</Icon>
+          <Text>Back</Text>
+        </Button>
       </animated.div>
     </Page>
   );
