bace9233696f578d14b895d1665de12a350b7a78
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-06-15 17:39:53 +0200

    Fixed about page animations

---
+++ src/Components/Library/LibraryWrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const LibraryWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 5px;
+`;
+
+export default LibraryWrapper;
--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -6,6 +6,7 @@ import Text from "../Components/Text/Text";
 import Button from "../Components/Button/Button";
 import Link from "../Components/Link/Link";
 import Library from "../Components/Library/Library";
+import LibraryWrapper from "../Components/Library/LibraryWrapper";
 import type { FC } from "react";
 
 const About: FC = (): JSX.Element => {
@@ -13,13 +14,6 @@ const About: FC = (): JSX.Element => {
     from: { opacity: 0, transform: "translateY(200%)" },
     to: { opacity: 1, transform: "translateY(0%)" },
     config: { tension: 250 },
-  });
-
-  const libraries = useSpring({
-    from: { opacity: 0, transform: "scale(0)" },
-    to: { opacity: 1, transform: "scale(1)" },
-    config: { tension: 250 },
-    delay: 100,
   });
 
   return (
@@ -36,54 +30,31 @@ const About: FC = (): JSX.Element => {
         <Icon fontSize={5}>&#xE946;</Icon>
         <Header fontSize={5}>About</Header>
         <Header fontSize={2}>Used libraries:</Header>
-        <animated.div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "5px",
-            flexWrap: "wrap",
-            maxWidth: "50%",
-            ...libraries,
-          }}
-        >
+        <LibraryWrapper>
           <Library>React</Library>
           <Library>Emotion</Library>
           <Library>react-spring</Library>
           <Library>react-router-dom</Library>
-        </animated.div>
+        </LibraryWrapper>
         <Header fontSize={2}>Used icons:</Header>
-        <animated.div style={libraries}>
-          <Library>Microsoft Fluent UI Icons</Library>
-        </animated.div>
+        <Library>Microsoft Fluent UI Icons</Library>
         <Header fontSize={2}>Useful links:</Header>
-        <animated.div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "10px",
-            ...libraries,
-          }}
-        >
-          <Link href="https://github.com/MateuszPerczak/tictactoe">
-            <Icon>&#xE167;</Icon>
-            <Text>This project</Text>
-          </Link>
-          <Link href="https://github.com/MateuszPerczak">
-            <Icon>&#xE167;</Icon>
-            <Text>Github</Text>
-          </Link>
-          <Link href="https://mateuszperczak.github.io/">
-            <Icon>&#xE12B;</Icon>
-            <Text>Website</Text>
-          </Link>
-          <Button to="/">
-            <Icon>&#xE10F;</Icon>
-            <Text>Back</Text>
-          </Button>
-        </animated.div>
+        <Link href="https://github.com/MateuszPerczak/tictactoe">
+          <Icon>&#xE167;</Icon>
+          <Text>This project</Text>
+        </Link>
+        <Link href="https://github.com/MateuszPerczak">
+          <Icon>&#xE167;</Icon>
+          <Text>Github</Text>
+        </Link>
+        <Link href="https://mateuszperczak.github.io/">
+          <Icon>&#xE12B;</Icon>
+          <Text>Website</Text>
+        </Link>
+        <Button to="/">
+          <Icon>&#xE10F;</Icon>
+          <Text>Back</Text>
+        </Button>
       </animated.div>
     </Page>
   );
