362806155dfc1833f49bcea5a3fb9da15482d19a
Mateusz Perczak <mateusz.perczak.mp@gmail.com>
2023-07-09 18:28:44 +0200

    Updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,8 +17,9 @@ export default defineConfig({
   },
   plugins: [
     react({
+      jsxImportSource: "@emotion/react",
       babel: {
-        plugins: ["@emotion"],
+        plugins: ["@emotion/babel-plugin"],
       },
     }),
     eslintPlugin(),
